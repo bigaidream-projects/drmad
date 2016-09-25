0700ddec95a3aedf914c7ccf4243018bbea0c5a8
taineleau <taineleau@gmail.com>
2016-09-25 23:47:17 +0800

    code everything as in optim, but loss is still mass.

--- CIFAR10/cifar10_L2.lua
+++ CIFAR10/cifar10_L2.lua
@@ -18,15 +18,15 @@ local c = require 'trepl.colorize'
 
 opt = lapp[[
    -s,--save            (default "logs")    subdirectory to save logs
-   -b,--batchSize         (default 64)      batch size
-   -r,--learningRate      (default 0.0001)      learning rate
+   -b,--batchSize         (default 128)      batch size
+   -r,--learningRate      (default 1)      learning rate
    --learningRateDecay      (default 1e-7)    learning rate decay
    --weightDecay          (default 0.0005)    weightDecay
    -m,--momentum          (default 0.9)       momentum
    --epoch_step         (default 25)      epoch step
    --model            (default vgg)       model name
    --max_epoch          (default 300)       maximum number of iterations
-   --backend            (default nn)        backend
+   --backend            (default cunn)        backend
    --mode             (default L2)        L2/L1/learningrate
    --type             (default cuda)      cuda/float/cl
    --numMeta            (default 3)         #episode
@@ -38,6 +38,96 @@ print(c.blue "==> " .. "parameter:")
 print(opt)
 
 grad.optimize(true)
+
+
+local function sgd_m(opfunc, x, config, state)
+   -- (0) get/update state
+   local config = config or {}
+   local state = state or config
+   local lr = config.learningRate or 1e-3
+   local lrd = config.learningRateDecay or 0
+   local wd = config.weightDecay or 0
+   local mom = config.momentum or 0
+   local damp = config.dampening or mom
+   local nesterov = config.nesterov or false
+   local lrs = config.learningRates
+   local wds = config.weightDecays
+   local forward_only = config.forward_only or nil
+   state.evalCounter = state.evalCounter or 0
+   local nevals = state.evalCounter
+   assert(not nesterov or (mom > 0 and damp == 0), "Nesterov momentum requires a momentum and zero dampening")
+
+
+   --print(config)
+   -- (1) evaluate f(x) and df/dx
+   local fx,dfdx = opfunc(x)
+
+   --print(dfdx)
+
+   -- (2) weight decay with single or individual parameters
+   if wd ~= 0 then
+      for i = 1, #dfdx do
+         dfdx[i]:add(wd, x[i])
+      end
+   elseif wds then
+      if not state.decayParameters then
+         state.decayParameters = torch.Tensor():typeAs(x):resizeAs(dfdx)
+      end
+      state.decayParameters:copy(wds):cmul(x)
+      dfdx:add(state.decayParameters)
+   end
+
+   -- (3) apply momentum
+   if mom ~= 0 then
+      if not state.dfdx then
+         state.dfdx = {}
+         for i = 1, #dfdx do
+            state.dfdx[i] = torch.Tensor():typeAs(dfdx[i]):resizeAs(dfdx[i]):copy(dfdx[i])
+         end
+      else
+         for i = 1, #dfdx do
+            state.dfdx[i]:mul(mom):add(1-damp, dfdx[i])
+         end
+      end
+      if nesterov then
+         dfdx:add(mom, state.dfdx)
+      else
+         dfdx = state.dfdx
+      end
+   end
+
+   -- (4) learning rate decay (annealing)
+   local clr = lr / (1 + nevals*lrd)
+
+   -- (5) parameter update with single or individual learning rates
+   if lrs then
+      if not state.deltaParameters then
+         state.deltaParameters = {}
+         for i = 1, #dfdx do
+            state.deltaParameters[i] = torch.Tensor():typeAs(x[i]):resizeAs(dfdx[i])
+         end
+      end
+      for i = 1, #dfdx do
+         state.deltaParameters[i]:copy(lrs[i]):cmul(dfdx[i])
+         x[i]:add(-clr[i], state.deltaParameters[i])
+      end
+   else
+      for i = 1, #dfdx do
+         x[i]:add(-clr, dfdx[i])
+      end
+   end
+
+   -- (6) update evaluation counter
+   state.evalCounter = state.evalCounter + 1
+
+   -- return x*, f(x) before optimization
+--   if not forward_only then
+--      return x, {fx}
+--   end
+
+
+   return x, {fx}
+end
 
 -- Load in MNIST
 
@@ -122,12 +212,12 @@ local function init(iter)
       local Lossf = grad.nn.CrossEntropyCriterion()
 
       -- define training function
-      local function fTrain(params, x, y, y_label)
+      local function fTrain(params, x, y)
          --print(params.elementary)
          --print(params.l2)
          local prediction = modelf(params.elementary, x)
-         local penalty = L2_norm(params.elementary, params.l2)
-         return Lossf(prediction, y) + penalty, prediction
+         --local penalty = L2_norm(params.elementary, params.l2)
+         return Lossf(prediction, y), prediction
       end
 
       dfTrain = grad(fTrain)
@@ -135,7 +225,6 @@ local function init(iter)
       -- a simple unit test
       local X = cast(torch.Tensor(4, 3, 32, 32):fill(0.5))
       local Y = cast(torch.Tensor(1, 4):fill(0))
-      local y_label =cast(torch.IntTensor{1,2,3,4})
 
       all_params = {
          elementary = params,
@@ -143,7 +232,7 @@ local function init(iter)
          velocity = params_velocity
       }
 
-      local dparams, l, p = dfTrain(all_params, X, Y, y_label)
+      local dparams, l, p = dfTrain(all_params, X, Y)
 
       if (l) then
         print(c.green '    Auto Diff works!')
@@ -174,6 +263,14 @@ local function gradProj(params, input, target, Proj, dV)
 
 end
 
+
+local optimState = {
+   learningRate = opt.learningRate,
+   weightDecay = opt.weightDecay,
+   momentum = opt.momentum,
+   learningRateDecay = opt.learningRateDecay,
+}
+
 local function train_meta(iter)
 
     -----------------------------------
@@ -182,29 +279,48 @@ local function train_meta(iter)
 
     -- Train a neural network to get final parameters
 
-   for epoch = 1, opt.max_epoch do
-      print(c.blue '==>' ..' Meta episode #' .. iter .. ', Training epoch #' .. epoch)
-      for i, inputs, targets in trainset:subiter(opt.batchSize) do
+    local iter_num = torch.floor(trainset:size() / opt.batchSize)
 
-         local function makesample(inputs, targets)
-            local t_ = torch.FloatTensor(targets:size(1), 10):zero()
-            for j = 1, targets:size(1) do
-               t_[targets[j]] = 1
-            end
-            return cast(inputs), cast(t_)
+--    print(iter_num)
+--
+--    sys.sleep(100)
+
+    local grads, loss, prediction
+
+
+    for epoch = 1, opt.max_epoch do
+      print(c.blue '==>' ..' Meta episode #' .. iter .. ', Training epoch #' .. epoch)
+      for i = 1, iter_num do
+         local inputs, targets = trainset:index(torch.LongTensor():range((i - 1) * opt.batchSize + 1, i * opt
+         .batchSize))
+
+         local X, Y = cast(inputs), cast(targets)
+--
+--         local grads, loss, prediction = dfTrain(all_params, X, Y)
+
+
+         local feval = function(x)
+            if x~=params then params:copy(x) end
+            grads, loss, prediction = dfTrain(all_params, X, Y)
+            confusionMatrix:batchAdd(prediction, Y)
+            return loss, grads.elementary
          end
 
-         local X, Y = cast(inputs), cast(targets)--makesample(inputs, targets)
-         local grads, loss, prediction = dfTrain(all_params, X, Y)
+         -- use optim's implementation
+         sgd_m(feval, params, optimState)
+
 
          -- update parameter
-         for i = 1, #grads do
-            params_velocity[i] = params_velocity[i]:mul(opt.learningRateDecay) - grads[i]:mul(1 - opt.learningRateDecay)
-            params[i] = params[i] + opt.learningRate * params_velocity[i]
-         end
+--         for j = 1, #grads.elementary do
+--            -- add weight dacay, i.e. l2 norm
+--            grads.elementary[j]:add(opt.weightDecay, params[j])
+--            params_velocity[j] = params_velocity[j]:mul(opt.learningRateDecay) - grads.elementary[j]:mul(1 - opt.learningRateDecay)
+--            params[j] = params[j] + opt.learningRate * params_velocity[j]
+--         end
+----
 
          -- Log performance:
-         confusionMatrix:batchAdd(prediction, Y)
+--         confusionMatrix:batchAdd(prediction, Y)
          if i % 100 == 0 then
             print("Epoch "..epoch)
             print(confusionMatrix)
@@ -213,6 +329,7 @@ local function train_meta(iter)
             end
          end
          print(c.red 'loss: ', loss)
+         --break
       end
    end
 
@@ -224,7 +341,51 @@ local function train_meta(iter)
    -- [[Reverse mode hyper-parameter training:
    -- to get gradient w.r.t. hyper-parameters]]
    -----------------------
-    dHyperProj = grad(gradProj)
+
+
+    --- obtain on validation
+
+
+
+
+
+--    for i = 1, iter_num do
+--       local inputs, targets = trainset:index(torch.LongTensor():range((i - 1) * opt.batchSize + 1, i * opt.batchSize))
+--
+--       local X, Y = cast(inputs), cast(targets)
+--
+--
+--       local feval = function(x)
+--          if x~=params then params:copy(x) end
+--          grads.elementary:zero()
+--          grads, loss, prediction = dfTrain(all_params, X, Y)
+--          confusionMatrix:batchAdd(prediction, Y)
+--          return loss, grads.elementary
+--       end
+--       -- update parameter
+--
+--       sgd_m(feval, params, optimState)
+--
+--
+----       for i = 1, #grads do
+----          params_velocity[i] = params_velocity[i]:mul(opt.learningRateDecay) - grads[i]:mul(1 - opt.learningRateDecay)
+----          params[i] = params[i] + opt.learningRate * params_velocity[i]
+----       end
+----
+----       -- Log performance:
+----       confusionMatrix:batchAdd(prediction, Y)
+--       if i % 50 == 0 then
+--          print("Epoch "..epoch)
+--          print(confusionMatrix)
+--          if i % 1000 == 0 then
+--             confusionMatrix:zero()
+--          end
+--       end
+----       print(c.red 'loss: ', loss)
+--       --break
+--    end
+--
+--    dHyperProj = grad(gradProj)
 
 
 
