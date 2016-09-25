258d2009b5ee840409d716e3228a336f99e308d2
taineleau <taineleau@gmail.com>
2016-09-25 19:59:18 +0800

    seems do not converge ...

--- CIFAR10/cifar10_L2.lua
+++ CIFAR10/cifar10_L2.lua
@@ -16,24 +16,22 @@ local xlua = require 'xlua'
 local c = require 'trepl.colorize'
 
 
---local debugger = require 'fb.debugger'
-
 opt = lapp[[
-   -s,--save                  (default "logs")      subdirectory to save logs
-   -b,--batchSize             (default 64)          batch size
-   -r,--learningRate          (default 0.0001)        learning rate
-   --learningRateDecay        (default 1e-7)      learning rate decay
-   --weightDecay              (default 0.0005)      weightDecay
-   -m,--momentum              (default 0.9)         momentum
-   --epoch_step               (default 25)          epoch step
-   --model                    (default vgg)         model name
-   --max_epoch                (default 300)           maximum number of iterations
-   --backend                  (default nn)            backend
-   --mode                     (default L2)            L2/L1/learningrate
-   --type                     (default cuda)          cuda/float/cl
-   --numMeta                  (default 3)             #episode
-   --hLR                      (default 0.0001)       learningrate of hyperparameter
-   --initHyper                (default 0.001)        initial value for hyperparameter
+   -s,--save            (default "logs")    subdirectory to save logs
+   -b,--batchSize         (default 64)      batch size
+   -r,--learningRate      (default 0.0001)      learning rate
+   --learningRateDecay      (default 1e-7)    learning rate decay
+   --weightDecay          (default 0.0005)    weightDecay
+   -m,--momentum          (default 0.9)       momentum
+   --epoch_step         (default 25)      epoch step
+   --model            (default vgg)       model name
+   --max_epoch          (default 300)       maximum number of iterations
+   --backend            (default nn)        backend
+   --mode             (default L2)        L2/L1/learningrate
+   --type             (default cuda)      cuda/float/cl
+   --numMeta            (default 3)         #episode
+   --hLR              (default 0.0001)     learningrate of hyperparameter
+   --initHyper          (default 0.001)      initial value for hyperparameter
 ]]
 
 print(c.blue "==> " .. "parameter:")
@@ -50,116 +48,132 @@ local confusionMatrix = optim.ConfusionMatrix(classes)
 print(c.blue '    completed!')
 
 
-local predict, model, modelf, dfTrain, params, initParams, params_l2
-local hyper_params = {}
+local predict, model, modelf, dfTrain, params, all_params, initParams, finalParams, params_l2, params_velocity
+local params_proj, dHyperProj
 
 
 local function cast(t)
-    if opt.type == 'cuda' then
-        require 'cunn'
-        return t:cuda()
-    elseif opt.type == 'float' then
-        return t:float()
-    elseif opt.type == 'cl' then
-        require 'clnn'
-        return t:cl()
-    else
-        error('Unknown type '..opt.type)
-    end
+   if opt.type == 'cuda' then
+      require 'cunn'
+      return t:cuda()
+   elseif opt.type == 'float' then
+      return t:float()
+   elseif opt.type == 'cl' then
+      require 'clnn'
+      return t:cl()
+   else
+      error('Unknown type '..opt.type)
+   end
 end
 
+
+-- create params that has a same size as elementary parameters
+local function full_create(params)
+   local full_params = {}
+   for i = 1, #params do
+      full_params[i] = params[i]:clone():fill(0)
+   end
+   return full_params
+end
+
+-- create params that has a same size as elementary weight parameters
+-- i.e. ignore bias parameters
 local function L2_norm_create(params, initHyper)
-    local hyper_L2 = {}
-    for i = 1, #params do
-        -- dimension = 1 is bias, do not need L2_reg
-        if (params[i]:nDimension() > 1) then
-            hyper_L2[i] = params[i]:clone():fill(initHyper)
-        end
-    end
-    return hyper_L2
+   local hyper_L2 = {}
+   for i = 1, #params do
+      -- dimension = 1 is bias, do not need L2_reg
+      if (params[i]:nDimension() > 1) then
+        hyper_L2[i] = params[i]:clone():fill(initHyper)
+      end
+   end
+   return hyper_L2
 end
-
 
 local function L2_norm(params, params_l2)
---    print(params_l2, params[1])
---    local penalty = torch.sum(torch.cmul(params[1], params_l2[1]))
---    local penalty = torch.sum(params[1])
-    local penalty = 0
-     for i = 1, #params do
-         --dimension = 1 is bias, do not need L2_reg
-         if (params[i]:nDimension() > 1) then
---             print('it works!'..i)
---             print(params[i])
---             print(params_l2[i])
-             penalty = penalty + torch.sum(torch.cmul(torch.cmul(params[i], params[i]), params_l2[i]))
---             print("finish!")
-         end
-     end
+--   local penalty = torch.sum(params[1]) * params_l2[1]
+   local penalty = 0
+   for i = 1, #params do
+       --dimension = 1 is bias, do not need L2_reg
+       if (params[i]:nDimension() > 1) then
+         --print(i)
+         penalty = penalty + torch.sum(torch.cmul(params[i], params_l2[i]))
+       end
+   end
     return penalty
 end
 
 
 local function init(iter)
+   ----
+   --- build VGG net.
+   ----
 
-    ----
-    --- build VGG net.
-    ----
+   if iter == 1 then
+      -- load model
+      print(c.blue '==>' ..' configuring model')
+      model = cast(dofile(root .. 'models/'..opt.model..'.lua'))
+      -- cast a model using functionalize
+      modelf, params = grad.functionalize(model)
 
-    if iter == 1 then
-        -- load model
-        print(c.blue '==>' ..' configuring model')
-        model = cast(dofile(root .. 'models/'..opt.model..'.lua'))
-        -- cast a model using functionalize
-        modelf, params = grad.functionalize(model)
+      params_l2 = L2_norm_create(params, opt.initHyper)
+      params_velocity = full_create(params)
+      params_proj = full_create(params)
 
-        params_l2 = L2_norm_create(params, opt.initHyper)
+      local Lossf = grad.nn.MSECriterion()
 
-        local Lossf = grad.nn.MSECriterion()
+      -- define training function
+      local function fTrain(params, x, y)
+         --print(params.elementary)
+         --print(params.l2)
+         local prediction = modelf(params.elementary, x)
+         local penalty = L2_norm(params.elementary, params.l2)
+         return Lossf(prediction, y) + penalty, prediction
+      end
 
-        local function fTrain(params, x, y)
---            print(params.p1)
---            print(params.p2)
-            local prediction = modelf(params.p2, x)
-            local penalty = L2_norm(params.p2, params.p1)
-            return Lossf(prediction, y) + penalty
-        end
+      dfTrain = grad(fTrain)
 
-        dfTrain = grad(fTrain)
+      -- a simple unit test
+      local X = cast(torch.Tensor(4, 3, 32, 32):fill(0.5))
+      local Y = cast(torch.Tensor(4, 10):fill(0))
 
-        -- a simple unit test
-        local X = cast(torch.Tensor(64, 3, 32, 32):fill(0.5))
-        local Y = cast(torch.Tensor(64, 10):fill(0))
+      all_params = {
+         elementary = params,
+         l2 = params_l2,
+         velocity = params_velocity
+      }
 
-        local p = {
-            p1 = params_l2,
-            p2 = params
-        }
+      local dparams, l, p = dfTrain(all_params, X, Y)
 
-        local dparams, l = dfTrain(p, X, Y)
+      if (l) then
+        print(c.green '    Auto Diff works!')
+      end
 
-        if (l) then
-            print(c.green '    Auto Diff works!')
-        end
+      print(c.blue '    completed!')
+   end
 
-        print(c.blue '    completed!')
-    end
+   print(c.blue '==>' ..' initializing model')
+   --print(params[1])
+   utils.MSRinit(model)
+   --print(params[1])
 
-    print(c.blue '==>' ..' initializing model')
-    utils.MSRinit(model)
+   print(c.blue '    completed!')
 
-
-    print(c.blue '    completed!')
-
-    -- copy initial weights for later computation
-    initParams = utils.deepcopy(params)
-
+   -- copy initial weights for later computation
+   initParams = utils.deepcopy(params)
 end
 
 
+local function gradProj(params, input, target, Proj, dV)
+--   local grads, loss, prediction = dfTrain(params, input, target)
+--   proj_1 = proj_1 + torch.cmul(grads.W[1] , DV_1)
+--   proj_2 = proj_2 + torch.cmul(grads.W[2] , DV_2)
+--   proj_3 = proj_3 + torch.cmul(grads.W[3] , DV_3)
+--   local loss = torch.sum(proj_1) + torch.sum(proj_2) + torch.sum(proj_3)
+--   return loss
+
+end
+
 local function train_meta(iter)
-    --[[
-    One meta-iteration to get directives w.r.t. hyperparameters
-    ]]
 
     -----------------------------------
     -- [[Meta training]]
@@ -167,44 +181,49 @@ local function train_meta(iter)
 
     -- Train a neural network to get final parameters
 
+   for epoch = 1, opt.max_epoch do
+      print(c.blue '==>' ..' Meta episode #' .. iter .. ', Training epoch #' .. epoch)
+      for i, inputs, targets in trainset:subiter(opt.batchSize) do
 
-    for epoch = 1, opt.max_epoch do
-        print(c.blue '==>' ..' Meta episode #' .. iter .. ', Training epoch #' .. epoch)
-        for i, inputs, targets in trainset:subiter(opt.batchSize) do
-
-            local function makesample(inputs, targets)
-                local t_ = torch.FloatTensor(targets:size(1), 10):zero()
-                for j = 1, targets:size(1) do
-                    t_[targets[j]] = 1
-                end
-                return inputs:cuda(), t_:cuda()
+         local function makesample(inputs, targets)
+            local t_ = torch.FloatTensor(targets:size(1), 10):zero()
+            for j = 1, targets:size(1) do
+               t_[targets[j]] = 1
             end
+            return cast(inputs), cast(t_)
+         end
 
-            local p = {
-                p1 = params_l2,
-                p2 = params
-            }
+         local X, Y = makesample(inputs, targets)
+         local grads, loss, prediction = dfTrain(all_params, X, Y)
 
-            local grads, loss = dfTrain(p, makesample(inputs, targets))
+         -- update parameter
+         for i = 1, #grads do
+            params_velocity[i] = params_velocity[i]:mul(opt.learningRateDecay) - grads[i]:mul(1 - opt.learningRateDecay)
+            params[i] = params[i] + opt.learningRate * params_velocity[i]
+         end
 
-            for i = 1, #grads do
-                params[i] = params[i] + opt.learningRate * grads[i]
-            end
+         -- Log performance:
+         confusionMatrix:batchAdd(prediction, Y)
+         if i % 1000 == 0 then
+            print("Epoch "..epoch)
+            print(confusionMatrix)
+            confusionMatrix:zero()
+         end
+         print(c.red 'loss: ', loss)
+      end
+   end
 
-            print(c.red 'loss: ', loss)
+   -- copy final parameters after convergence
+   finalParams = utils.deepcopy(params)
+   finalParams = nn.utils.recursiveCopy(finalParams, params)
 
-        end
-    end
+   -----------------------
+   -- [[Reverse mode hyper-parameter training:
+   -- to get gradient w.r.t. hyper-parameters]]
+   -----------------------
+    dHyperProj = grad(gradProj)
 
-    -- copy final parameters after convergence
-    local finalParams = utils.deepcopy(params)
-    finalParams = nn.utils.recursiveCopy(finalParams, params)
 
-
-    -----------------------
-    -- [[Reverse mode hyper-parameter training:
-    -- to get gradient w.r.t. hyper-parameters]]
-    -----------------------
 
 end
 
